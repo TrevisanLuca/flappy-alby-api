dc7f9578a84144dce833b702a113b4fc83f3f661
Luca Trevisan <trevisanluca86@gmail.com>
2022-03-30 19:38:15 +0200

    ef

---
+++ FlappyAlby.API/Data/FlappyDbContext.cs
@@ -0,0 +1,10 @@
+﻿using FlappyAlby.API.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlappyAlby.API.Data
+{
+    public class FlappyDbContext : DbContext
+    {
+        public DbSet<Player> Players { get; set; } = null!;
+    }
+}
--- FlappyAlby.API/Program.cs
+++ FlappyAlby.API/Program.cs
@@ -1,8 +1,10 @@
 using FlappyAlby.API.Abstract;
+using FlappyAlby.API.Data;
 using FlappyAlby.API.Options;
 using FlappyAlby.API.Readers;
 using FlappyAlby.API.Repository;
 using FlappyAlby.API.Writers;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +24,7 @@ builder.Services
 builder.Services.AddSingleton<IReader, SQLReader>();
 builder.Services.AddSingleton<IWriter, SQLWriter>();
 builder.Services.AddSingleton<IRankingRepository, RankingRepository>();
+builder.Services.AddDbContext<FlappyDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDatabase")));
 
 var app = builder.Build();
 
